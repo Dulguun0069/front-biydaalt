8da58be63c959f99b5919a46bdcc8a21a2d718b2
Dulguun0069 <dulguunu0609@gmail.com>
2025-03-28 00:01:05 +0800

    Initial commit

---
+++ frontend/my-app/src/component/ProductList.jsx
@@ -0,0 +1,17 @@
+    import React from "react";
+    import { Grid } from "@mui/material";
+    import ProductCard from "./ProductCard";
+
+    const ProductList = ({ products, addToCart }) => {
+    return (
+        <Grid container spacing={3}>
+        {products.map((product) => (
+            <Grid item key={product.id} xs={12} sm={6} md={4}>
+            <ProductCard product={product} addToCart={addToCart} />
+            </Grid>
+        ))}
+        </Grid>
+    );
+    };
+
+    export default ProductList; 
