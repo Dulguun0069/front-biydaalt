0a46d4197ec0a1baec19015ded7e87960e666963
Dulguun0069 <dulguunu0609@gmail.com>
2025-03-28 00:05:37 +0800

    Initial commit

---
+++ frontend/my-app/src/component/products.jsx
@@ -0,0 +1,39 @@
+    const products = [
+        {
+        id: 1,
+        description: "Product 1-1",
+        name: "Product 1-1",
+        price: 19.99,
+        colors: ["red", "blue", "green", "white"],
+        picture: "https://t3.ftcdn.net/jpg/10/00/82/64/360_F_1000826459_SkULxmIvvHot6n271iimrZvcUekncxsw.jpg",
+        },
+        {
+        id: 2,
+        name: "Product 1-2",
+        price: 24.99,
+        colors: ["yellow", "purple", "white", "brown"],
+        picture: "https://t3.ftcdn.net/jpg/10/00/82/64/360_F_1000826459_SkULxmIvvHot6n271iimrZvcUekncxsw.jpg",
+        },
+        {
+        id: 3,
+        name: "Product 2-1",
+        price: 29.99,
+        colors: ["black", "pink", "orange", "green"],
+        picture: "https://t3.ftcdn.net/jpg/10/00/82/64/360_F_1000826459_SkULxmIvvHot6n271iimrZvcUekncxsw.jpg",
+        },
+        {
+        id: 4,
+        name: "Product 2-2",
+        price: 15.99,
+        colors: ["brown", "gray", "teal", "black"],
+        picture: "https://t3.ftcdn.net/jpg/10/00/82/64/360_F_1000826459_SkULxmIvvHot6n271iimrZvcUekncxsw.jpg",
+        },
+        {
+        id: 5,
+        name: "Product 3",
+        price: 39.99,
+        colors: ["cyan", "magenta", "lime", "indigo"],
+        picture: "https://t3.ftcdn.net/jpg/10/00/82/64/360_F_1000826459_SkULxmIvvHot6n271iimrZvcUekncxsw.jpg",
+        },
+    ];
+    export default products;
